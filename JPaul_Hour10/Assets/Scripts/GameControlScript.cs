d8026f4f3068db72f578de42575e80748fa08cf3
jpnerf1-hub <jpnerf1@gmail.com>
2025-11-07 00:53:50 -0800

    Created ChaosBall Game

---
+++ JPaul_Hour10/Assets/Scripts/GameControlScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Note you can define several variables of the same type on one line
+    public GameObject blueBall, greenBall, redBall, orangeBall;
+    private bool isGameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If all four balls are not (!) existent then game is over
+        isGameOver = !blueBall && !greenBall && !redBall && !orangeBall;
+    }
+
+    void OnGUI()
+    {
+        if (isGameOver)
+        {
+            // Corrected line 25: declared as Rect type with variable name 'rect'
+            Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
+            GUI.Box(rect, "Game Over");
+            Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
+            GUI.Label(rect2, "Good Job!");
+        }
+    }
+}
